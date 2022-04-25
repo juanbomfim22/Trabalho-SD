eeebec4112d8acb5d34d0c4a345be3d6236fcaa1
LeleSilveira <74831693+LeleSilveira@users.noreply.github.com>
2022-04-24 22:37:04 -0300

    Update Chat.java

--- ChatRabbitMQ/src/main/java/br/ufs/dcomp/rabbitmq/chat/Chat.java
+++ ChatRabbitMQ/src/main/java/br/ufs/dcomp/rabbitmq/chat/Chat.java
@@ -1,9 +1,3 @@
-package br.ufs.dcomp.rabbitmq.chat;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,10 +38,10 @@ public class Chat {
 			connection = factory.newConnection();
 		}
 		channel.put("mensagens", connection.createChannel());
-		//channel.put("arquivos", connection.createChannel());
+		channel.put("arquivos", connection.createChannel());
 
 		channel.get("mensagens").queueDeclare(username, false, false, false, null);
-		//channel.get("arquivos").queueDeclare(username, false, false, false, null);
+		channel.get("arquivos").queueDeclare(username, false, false, false, null);
 	}
 
 	public static Map<String, Channel> getChannel() {
@@ -88,25 +82,43 @@ public class Chat {
 				String data = contatoMensagem.getData();
 				String hora = contatoMensagem.getHora();
 				String grupo = contatoMensagem.getGrupo();
+
+				byte[] corpoMensagem = conteudo.getCorpo().toByteArray();
+				
+				System.out.print("\033[2K"); // Erase line content
+				
+				
+				String strMensagem = new String(corpoMensagem, "UTF-8"); // FORMATAR DISPLAY DE MENSAGEM
+				String grupoEmissor = grupo.equals("") ? emissor : emissor + "#" + grupo;
+				System.out.println("\r" + "(" + data + " às " + hora + ") " + grupoEmissor + " diz: " + strMensagem);
+			}
+		};
+			Consumer consumerFiles = new DefaultConsumer(channel.get("arquivos")) {
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				MensagemProto.Mensagem contatoMensagem = MensagemProto.Mensagem.parseFrom(body);
+				MensagemProto.Conteudo conteudo = contatoMensagem.getConteudo();
+
+				String emissor = contatoMensagem.getEmissor();
+				String data = contatoMensagem.getData();
+				String hora = contatoMensagem.getHora();
+				String grupo = contatoMensagem.getGrupo();
 				String nome = conteudo.getNome();
 
 				byte[] corpoMensagem = conteudo.getCorpo().toByteArray();
 				
 				System.out.print("\033[2K"); // Erase line content
 				
-				// Descomentar essas 3 e comentar as 3 debaixo pra testar o arquivo envios
 				
 				Path path = Paths.get("\\tmp\\Downloads\\");
 				Files.write(path, corpoMensagem);
 				System.out.println("\r" + "(" + data + " às " + hora + ") " + "Arquivo \"" + nome + "\" recebido de @" + emissor + "!" );
-				
-//				String strMensagem = new String(corpoMensagem, "UTF-8"); // FORMATAR DISPLAY DE MENSAGEM
-//				String grupoEmissor = grupo.equals("") ? emissor : emissor + "#" + grupo;
-//				System.out.println("\r" + "(" + data + " às " + hora + ") " + grupoEmissor + " diz: " + strMensagem);
 			}
 		};
 		
 		channel.get("mensagens").basicConsume(username, true, consumer); // a fila tem o mesmo nome do username
+		channel.get("arquivos").basicConsume(username, true, consumer); // a fila tem o mesmo nome do username
+		
 	}
 
 	public void execute(ActionStrategy strategy, String fullLine) throws Exception {
