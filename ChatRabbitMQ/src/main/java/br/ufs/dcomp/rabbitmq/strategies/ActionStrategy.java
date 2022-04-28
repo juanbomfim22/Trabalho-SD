a10098c5a9e5ce246e83888831e65a6690d3d081
juanbomfim22 <juanbomfim22@gmail.com>
2022-04-28 15:49:06 -0300

    Removendo arquivo repetido

--- ChatRabbitMQ/src/main/java/br/ufs/dcomp/rabbitmq/strategies/ActionStrategy.java
+++
@@ -1,9 +0,0 @@
-package br.ufs.dcomp.rabbitmq.strategies;
-
-import com.rabbitmq.client.Channel;
-
-import br.ufs.dcomp.rabbitmq.Input;
-
-public interface ActionStrategy {
-	void run(Channel channel, Input input, String username) throws Exception; // retorna a nova seta do chat >>
-}
